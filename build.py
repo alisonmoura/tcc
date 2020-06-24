8973d49653d0da25b7000d1646d1a763bf6ed75c
Alison Moura <alison.oghino@gmail.com>
2020-06-24 19:44:07 -0400

    Fazendo script de build

---
+++ build.py
@@ -0,0 +1,15 @@
+import shutil
+import os
+
+if not os.path.isdir('dist'):
+    os.mkdir('dist')
+    print('Criando diretório do build')
+
+shutil.copy('./index.py', './dist/index.py')
+shutil.copy('./printer.py', './dist/printer.py')
+shutil.copy('./importer.py', './dist/importer.py')
+shutil.copy('./osvm.py', './dist/osvm.py')
+shutil.copy('./iforest.py', './dist/iforest.py')
+shutil.copy('./mcd.py', './dist/mcd.py')
+shutil.copy('./requirements.txt', './dist/requirements.txt')
+shutil.copytree('./datas', './dist/datas')
